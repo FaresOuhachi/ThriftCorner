cdca7dded78ff4c6090da3830dac3688cfdf4bac
Fares Ouhachi's Laptop <ouhachifares@gmail.com>
2024-11-13 17:32:41 +0100

    Version 1.0.1, TabBar Line deleted successfully

--- lib/screens/login_signup_screen.dart
+++ lib/screens/login_signup_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:thriftcorner/screens/signup_tab.dart';
 
 import 'login_tab.dart';
@@ -13,15 +11,6 @@ class LoginSignupScreen extends StatefulWidget {
 class _LoginSignupScreenState extends State<LoginSignupScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-  // Controllers for Login and Sign Up fields
-  final TextEditingController _loginEmailController = TextEditingController();
-  final TextEditingController _loginPasswordController = TextEditingController();
-  final TextEditingController _signupEmailController = TextEditingController();
-  final TextEditingController _signupPasswordController = TextEditingController();
-  final TextEditingController _signupUsernameController = TextEditingController();
 
   @override
   void initState() {
@@ -33,44 +22,6 @@ class _LoginSignupScreenState extends State<LoginSignupScreen>
   void dispose() {
     _tabController.dispose();
     super.dispose();
-  }
-
-  // Google Sign-In Logic
-  Future<void> _signInWithGoogle() async {
-    try {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      if (googleUser == null) return; // User canceled sign-in
-      final GoogleSignInAuthentication googleAuth = await googleUser
-          .authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-      await _auth.signInWithCredential(credential);
-      // Navigate to Home Screen
-    } catch (e) {
-      print("Google Sign-In Error: $e");
-    }
-  }
-
-  // Forgot Password Logic
-  Future<void> _resetPassword() async {
-    if (_loginEmailController.text.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Please enter your email to reset password."),
-        backgroundColor: Colors.red,
-      ));
-      return;
-    }
-    try {
-      await _auth.sendPasswordResetEmail(email: _loginEmailController.text);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Password reset email sent."),
-        backgroundColor: Colors.green,
-      ));
-    } catch (e) {
-      print("Forgot Password Error: $e");
-    }
   }
 
   @override
@@ -102,26 +53,35 @@ class _LoginSignupScreenState extends State<LoginSignupScreen>
               // Tabs
               Center(
                 child: Container(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.5,
+                  width: MediaQuery.of(context).size.width * 0.5,
                   padding: EdgeInsets.all(4),
                   decoration: BoxDecoration(
-                    color: Colors.grey[900],
+                    color: Color(0xFF595960).withOpacity(0.3),
                     borderRadius: BorderRadius.circular(30),
                   ),
                   child: TabBar(
                     controller: _tabController,
+                    dividerColor: Colors.transparent,
+                    dividerHeight: 0,
                     indicator: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(5),
+                      gradient: LinearGradient(
+                        colors: [Color(0xAABEE34F), Color(0xFFFFFFFF)],
+                        begin: Alignment(-1.5, -1.5),
+                        end: Alignment(1.5, 1.5),
+                      ),
+                      borderRadius: BorderRadius.circular(24),
                     ),
-                    labelColor: Colors.white,
-                    unselectedLabelColor: Colors.grey,
+                    labelColor: Colors.black,
+                    unselectedLabelColor: Color(0x66FFFFFF),
                     tabs: [
-                      Tab(text: 'Login'),
-                      Tab(text: 'Sign Up'),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Tab(text: 'Login'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Tab(text: 'Sign Up'),
+                      ),
                     ],
                   ),
                 ),
